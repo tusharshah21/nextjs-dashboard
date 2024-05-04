41b679c663ffbc1b3797b9de1dc05910f706ad2a
tushar <tusharkumarshah14394@gmail.com>
2024-05-04 12:21:11 +0530

    added authentication

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -125,7 +125,7 @@ export async function fetchFilteredInvoices(
       ORDER BY invoices.date DESC
       LIMIT ${ITEMS_PER_PAGE} OFFSET ${offset}
     `;
-
+    // console.log(invoice);
     return invoices.rows;
   } catch (error) {
     console.error('Database Error:', error);
